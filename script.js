eb7d32d519612225e75d41cb042499c39c0dc4da
zerop96 <omarpavon01@gmail.com>
2023-02-26 01:01:49 -0300

    Se agregaron las constantes y la matriz

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector("mensaje");
+
+let matrizCodigo = [
+  ["e", "enter"],
+  ["i", "ines"],
+  ["a", "ai"],
+  ["o", "ober"],
+  ["u", "ufat"],
+];
+console.table(matrizCodigo);
